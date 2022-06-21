e754e5649a52c533a499bc662215d81a6584ed8e
expl0r3rgu1 <salvaddimando@gmail.com>
2022-06-21 19:15:57 +0200

    Created Aggiungi Cliente event handler

--- mikasa.py
+++ mikasa.py
@@ -6,11 +6,11 @@ from credentials import *
 from windows import *
 
 db = mysql.connector.connect(
-    host = HOST,
-    user = USER,
-    passwd = PASSWORD,
-    database = DATABASE,
-    auth_plugin = AUTH_PLUGIN
+    host=HOST,
+    user=USER,
+    passwd=PASSWORD,
+    database=DATABASE,
+    auth_plugin=AUTH_PLUGIN
 )
 
 db_cursor = db.cursor()
@@ -21,13 +21,30 @@ window = default_window()
 while True:
     event, values = window.read()
     if event == 'Visualizza clienti':
-        db_cursor.execute("SELECT * FROM persone WHERE EXISTS (SELECT 1 FROM clienti WHERE clienti.cf_cliente = persone.cf)")
+        db_cursor.execute(
+            "SELECT * FROM persone WHERE EXISTS (SELECT 1 FROM clienti WHERE clienti.cf_cliente = persone.cf)")
         clienti = db_cursor.fetchall()
         window.close()
         window = clienti_window(clienti)
     elif event == 'Aggiungi cliente':
         window.close()
         window = aggiungi_cliente_window()
+
+        while True:
+            event, values = window.read()
+            if event == 'Conferma':
+                db_cursor.execute("INSERT INTO persone (cf, nome, cognome, telefono, email, via, civico, cap, città) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (
+                    values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8]))
+                db.commit()
+                db_cursor.execute("INSERT INTO clienti (cf_cliente, socio) VALUES (%s, 0)", (values[0],))
+                db.commit()
+                window.close()
+                window = default_window()
+                break
+            elif event == 'Annulla':
+                window.close()
+                window = default_window()
+                break
     elif event == 'Indietro':
         window.close()
         window = default_window()
