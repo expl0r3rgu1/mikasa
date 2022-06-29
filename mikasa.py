08b2cb5422a8f851c0c44328d82d1b8ed3b3744d
expl0r3rgu1 <salvaddimando@gmail.com>
2022-06-30 01:09:30 +0200

    Created Effettua ordine per cliente event handler

--- mikasa.py
+++ mikasa.py
@@ -5,6 +5,8 @@ import mysql.connector
 from credentials import *
 from windows import *
 from queries import *
+from datetime import date, timedelta
+import random
 
 db = mysql.connector.connect(
     host=HOST,
@@ -199,6 +201,123 @@ while True:
                 db_cursor.execute(QUERIES['Aggiungi negozio'], (values[0], values[1],
                                   values[2], values[3], values['-CAL-'], values['acquirente'][0], 1))
                 db.commit()
+                window.close()
+                window = default_window()
+                break
+            elif event == 'Annulla':
+                window.close()
+                window = default_window()
+                break
+    elif event == 'Effettua ordine per cliente':
+        db_cursor.execute(QUERIES['Visualizza clienti'])
+        clienti = db_cursor.fetchall()
+        db_cursor.execute("SELECT * FROM prodotti")
+        prodotti = db_cursor.fetchall()
+        db_cursor.execute("SELECT * FROM composizioni")
+        composizioni = db_cursor.fetchall()
+        db_cursor.execute("SELECT * FROM negozi")
+        negozi = db_cursor.fetchall()
+        window.close()
+        window = effettua_ordine_cliente_window(
+            clienti, prodotti, composizioni, negozi)
+        window['montaggio'].update(disabled=True)
+        window['indirizzo'].update(disabled=True)
+        window['negozio'].update(disabled=True)
+
+        while True:
+            event, values = window.read()
+            if event == 'spedizione':
+                if(values['spedizione'] == 'Con spedizione'):
+                    window['montaggio'].update(disabled=False)
+                    window['indirizzo'].update(disabled=False)
+                    window['negozio'].update(disabled=True)
+                else:
+                    window['montaggio'].update(disabled=True)
+                    window['indirizzo'].update(disabled=True)
+                    window['negozio'].update(disabled=False)
+            elif event == 'Conferma':
+                prodotti_acquistati = values['prodotti']
+                composizioni_acquistate = values['composizioni']
+
+                costo_totale = 0
+                peso_totale = 0
+                for prodotto in prodotti_acquistati:
+                    db_cursor.execute(
+                        'SELECT s.percentuale FROM sconti s WHERE s.cod_sconto = %s', (prodotto[7],))
+                    sconto = db_cursor.fetchone()
+                    if sconto is None:
+                        costo_totale += prodotto[2]
+                    else:
+                        costo_totale += (prodotto[2] * (1 - sconto[0] / 100))
+                    peso_totale += prodotto[6]
+
+                for composizione in composizioni_acquistate:
+                    db_cursor.execute(
+                        'SELECT * FROM prodotti WHERE EXISTS (SELECT * FROM composte WHERE cod_composizione = %s)', (composizione[0],))
+                    prodotti_composizione = db_cursor.fetchall()
+                    for prodotto in prodotti_composizione:
+                        db_cursor.execute(
+                            'SELECT s.percentuale FROM sconti s WHERE s.cod_sconto = %s', (prodotto[7],))
+                        sconto = db_cursor.fetchone()
+                        if sconto is None:
+                            costo_totale += prodotto[2]
+                        else:
+                            costo_totale += prodotto[2] * (1 - sconto[0] / 100)
+                        peso_totale += prodotto[6]
+
+                db_cursor.execute('SELECT * FROM tecnici_commerciali')
+                tecnici_commerciali = db_cursor.fetchall()
+
+                db_cursor.execute(
+                    'SELECT c.socio FROM clienti c WHERE c.cf_cliente = %s', (values['cliente'][0],))
+                is_socio = db_cursor.fetchone()[0]
+                if(is_socio):
+                    costo_totale *= 0.95
+
+                db_cursor.execute('INSERT INTO ordini (data_effettuazione, costo_totale, peso_totale, data_arrivo, cf_cliente, cf_tecnico_commerciale) VALUES (%s, %s, %s, %s, %s, %s)', (
+                    date.today(), costo_totale, peso_totale, date.today() + timedelta(days=5), values['cliente'][0], random.choice(tecnici_commerciali)[0]))
+                db.commit()
+
+                cod_ordine = db_cursor.lastrowid
+
+                for i, prodotto in enumerate(prodotti_acquistati):
+                    db_cursor.execute('INSERT INTO dettagli_prodotto (cod_ordine, cod_prodotto, quantità, prezzo_totale, peso_totale) VALUES (%s, %s, %s, %s, %s)', (cod_ordine, prodotto[0], int(values['quantità_prodotti'].split(
+                        ',')[i]), prodotto[2] * int(values['quantità_prodotti'].split(',')[i]), prodotto[6] * int(values['quantità_prodotti'].split(',')[i])))
+                    db.commit()
+
+                for i, composizione in enumerate(composizioni_acquistate):
+                    db_cursor.execute(
+                        'SELECT * FROM prodotti WHERE EXISTS (SELECT * FROM composte WHERE cod_composizione = %s)', (composizione[0],))
+                    prodotti_composizione = db_cursor.fetchall()
+                    costo_composizione = 0
+                    peso_composizione = 0
+                    for prodotto in prodotti_composizione:
+                        costo_composizione += prodotto[2]
+                        peso_composizione += prodotto[6]
+
+                    db_cursor.execute('INSERT INTO dettagli_composizione (cod_ordine, cod_composizione, quantità, prezzo_totale, peso_totale) VALUES (%s, %s, %s, %s, %s)', (
+                        cod_ordine, composizione[0], int(values['quantità_composizioni'].split(',')[i]), costo_composizione, peso_composizione))
+
+                if(values['spedizione'] == 'Con spedizione'):
+                    db_cursor.execute('SELECT * FROM tecnici')
+                    tecnici = db_cursor.fetchall()
+                    tecnico = random.choice(tecnici)
+                    db_cursor.execute(
+                        QUERIES['Effettua ordine con spedizione'], (cod_ordine, values['indirizzo'], tecnico))
+                    db.commit()
+                    if(values['montaggio'] == 'Con montaggio'):
+                        db_cursor.execute(
+                            QUERIES['Effettua ordine con montaggio'], (cod_ordine, values['indirizzo'], tecnico))
+                        db.commit()
+                    else:
+                        db_cursor.execute(
+                            QUERIES['Effettua ordine senza montaggio'], (cod_ordine, values['indirizzo'], tecnico))
+                        db.commit()
+                else:
+                    db_cursor.execute(
+                        QUERIES['Effettua ordine senza spedizione'], (cod_ordine, values['negozio'][0]))
+                    db.commit()
+
                 window.close()
                 window = default_window()
                 break
