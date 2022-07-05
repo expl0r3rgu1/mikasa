f3c81bc1b8d8ccec0a09d1c73ad67babcf5a99e9
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-07-05 18:49:18 +0200

    add query 'Visualizza 10 ordini più costosi cliente'

--- queries.py
+++ queries.py
@@ -52,6 +52,7 @@ QUERIES = {
     'Visualizza ordine più costoso': 'SELECT cod_ordine, costo_totale FROM ordini ORDER BY costo_totale DESC LIMIT 1',
     'Visualizza 10 ordini più costosi': 'SELECT cod_ordine, costo_totale FROM ordini ORDER BY costo_totale DESC LIMIT 10',
     'Visualizza ordine più costoso cliente': 'SELECT c.cf_cliente, o.cod_ordine, o.costo_totale FROM clienti c, ordini o WHERE c.cf_cliente = %s AND c.cf_cliente = o.cf_cliente ORDER BY costo_totale DESC LIMIT 1',
+    'Visualizza 10 ordini più costosi cliente': 'SELECT c.cf_cliente, o.cod_ordine, o.costo_totale FROM clienti c, ordini o WHERE c.cf_cliente = %s AND c.cf_cliente = o.cf_cliente ORDER BY costo_totale DESC LIMIT 10',
     'Visualizza sconti' : 'SELECT * FROM sconti',
     'Visualizza negozi' : 'SELECT * FROM negozi',
     'Visualizza acquirenti' : 'SELECT * FROM acquirenti',
