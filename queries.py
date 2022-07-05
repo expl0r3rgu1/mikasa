e64227a7f1e682723cbb0805bcbb8893db217e14
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-07-05 18:36:09 +0200

    correct query 'Visualizza 10 prodotti meno costosi'

--- queries.py
+++ queries.py
@@ -37,7 +37,7 @@ QUERIES = {
     'Visualizza prodotto meno acquistato': 'SELECT p.*, SUM(CASE WHEN p.cod_prodotto = d.cod_prodotto THEN d.quantità ELSE 0 END) AS quantità FROM prodotti p, dettagli_prodotto d WHERE p.cod_prodotto = d.cod_prodotto GROUP BY p.cod_prodotto ORDER BY quantità ASC LIMIT 1',
     'Visualizza prodotto più costoso': 'SELECT p.* FROM prodotti p ORDER BY prezzo DESC LIMIT 1',
     'Visualizza 10 prodotti più costosi' : 'SELECT p.* FROM prodotti p ORDER BY prezzo DESC LIMIT 10',
-    'Visualizza prodotto meno costoso': 'SELECT p.* FROM prodotti p ORDER BY prezzo DESC LIMIT 1',
+    'Visualizza prodotto meno costoso': 'SELECT p.* FROM prodotti p ORDER BY prezzo ASC LIMIT 1',
     'Visualizza 10 prodotti meno costosi' : 'SELECT p.* FROM prodotti p ORDER BY prezzo ASC LIMIT 10',
     'Visualizza alimento porzionato più costoso': 'SELECT a.* FROM alimenti a ORDER BY prezzo_porzionato DESC LIMIT 1',
     'Visualizza alimento confezionato più costoso': 'SELECT a.* FROM alimenti a ORDER BY prezzo_confezionato DESC LIMIT 1',
