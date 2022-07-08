6b1d59a4c9b595c6818c0b0d44aa16ffb981e32d
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-07-08 18:46:51 +0200

    change query 'Visualizza quantità magazzini prodotto'

--- queries.py
+++ queries.py
@@ -34,7 +34,7 @@ QUERIES = {
     'Visualizza 10 alimenti confezionati più costosi' : 'SELECT a.* FROM alimenti a ORDER BY prezzo_confezionato DESC LIMIT 10',
     'Visualizza 10 alimenti porzionati meno costosi' : 'SELECT a.* FROM alimenti a ORDER BY prezzo_porzionato ASC LIMIT 10',
     'Visualizza 10 alimenti confezionati meno costosi' : 'SELECT a.* FROM alimenti a ORDER BY prezzo_confezionato ASC LIMIT 10',
-    'Visualizza quantità magazzini prodotto': 'SELECT cod_negozio, quantità FROM quantità WHERE cod_prodotto = %s',
+    'Visualizza quantità magazzini prodotto': 'SELECT q.cod_negozio, p.cod_prodotto, p.tipo, q.quantità FROM quantità q, prodotti p WHERE q.cod_prodotto = %s AND q.cod_prodotto = p.cod_prodotto',
     'Visualizza prodotti terminati': 'SELECT q.cod_negozio, q.cod_prodotto, p.nome FROM quantità q, prodotti p WHERE q.cod_prodotto = p.cod_prodotto AND q.quantità = 0',
     'Visualizza personale': 'SELECT a.*, t.*, c.* FROM amministratori a, tecnici t, tecnici_commerciali c',
     'Visualizza 10 prodotti con sconto maggiore': 'SELECT p.cod_prodotto, p.nome, s.cod_sconto, s.percentuale FROM sconti s, prodotti p WHERE p.cod_sconto = s.cod_sconto GROUP BY p.cod_sconto ORDER BY s.percentuale DESC LIMIT 10',
